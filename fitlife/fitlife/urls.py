34d6faf3d5ef40fd010c7f0e326f535a08bc8d24
William-0907 <williamsotnac@gmail.com>
2025-04-11 21:46:22 +0800

    Second Commit

--- fitlife/fitlife/urls.py
+++ fitlife/fitlife/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainpage.views import home
+from mainpage.views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', welcome)
 ]
--- fitlife/mainpage/views.py
+++ fitlife/mainpage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 # Create your views here.
 
-def home(request):
-  return render(request, 'mainpage/base.html')
+def welcome(request):
+  return render(request, 'mainpage/welcome.html')
